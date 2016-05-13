852c7c5d97ca23bbdd3388fb028ba718304c17eb
heruiwoniou <heruiwoniou@qq.com>
2016-05-14 00:13:02 +0800

    'eslint'

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulp=require('gulp'),
 	eslint=require('gulp-eslint');
 
 gulp.task('lint',function(){
-	return gulp.src(['src/**/*.js'])
+	return gulp.src(['src/**/*.js','!node_modules/**'])
 		.pipe(eslint())
 		.pipe(eslint.format())
 		.pipe(eslint.failAfterError());
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { a } from './a';
 
 export default function (){
-	let NewName= [ 1, 2, 3 ];
-	console.log(a);
+	var NewName= [ 1, 2, 3 ];
+	console.log(NewName,a);
 }
